6ec8336ea8f3e8bd6d0e99a8f32d26a6571e5291
Mahtab Alam <mahtab@secondus.gamersdemokrasy.com>
2016-11-30 23:14:50 +0530

    All done

---
+++ app.js
@@ -0,0 +1,26 @@
+var app=angular.module('cancerRegistryApp',['mgcrea.ngStrap']);
+
+
+app.controller('formController',function($scope){
+     var formData={};
+});
+
+
+app.directive('capitalize', function() {
+    return {
+      require: 'ngModel',
+      link: function(scope, element, attrs, modelCtrl) {
+        var capitalize = function(inputValue) {
+          if (inputValue == undefined) inputValue = '';
+          var capitalized = inputValue.toUpperCase();
+          if (capitalized !== inputValue) {
+            modelCtrl.$setViewValue(capitalized);
+            modelCtrl.$render();
+          }
+          return capitalized;
+        }
+        modelCtrl.$parsers.push(capitalize);
+        capitalize(scope[attrs.ngModel]); // capitalize initial value
+      }
+    };
+  });
